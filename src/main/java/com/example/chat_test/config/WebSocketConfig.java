4da4003a005ee36051254a75dccf0a3a51262fcf
tmddnxx <lsw6878@naver.com>
2024-10-25 17:48:55 +0900

    [feat] : Stomp 채팅 테스트

--- src/main/java/com/example/chat_test/config/WebSocketConfig.java
+++ src/main/java/com/example/chat_test/config/WebSocketConfig.java
@@ -1,21 +1,23 @@
 package com.example.chat_test.config;
 
-import com.example.chat_test.handler.ChatHandler;
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
-import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
-import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.*;
 
 @Configuration
-@RequiredArgsConstructor
-@EnableWebSocket
-public class WebSocketConfig implements WebSocketConfigurer {
-
-    private final ChatHandler chatHandler;
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
-    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(chatHandler, "ws/chat").setAllowedOrigins("*");
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/sub");
+        registry.setApplicationDestinationPrefixes("/pub");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/chat")
+                .setAllowedOrigins("http://localhost:8080")
+                .withSockJS();
     }
 }
--- src/main/java/com/example/chat_test/controller/ChatController.java
+++ src/main/java/com/example/chat_test/controller/ChatController.java
@@ -1,17 +1,30 @@
 package com.example.chat_test.controller;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @Log4j2
+@RequiredArgsConstructor
 public class ChatController {
 
+    private final SimpMessagingTemplate messagingTemplate;
+
     @GetMapping("/chat")
-    public String chatGET(){
-        log.info("@ChatController, chat GET()");
+    private String getChat(){
         return "chat";
     }
 
+    @MessageMapping("/chat")
+    @SendTo("/sub/messages")
+    public String sendMessage(@Payload String message) {
+        log.info("받은 메시지: {}", message); // {} = 매개변수 포맷팅
+        return message; // 클라이언트에 메시지를 다시 브로드캐스팅
+    }
 }
